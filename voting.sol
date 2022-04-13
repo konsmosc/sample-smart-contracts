b235a83e8a68da5a3b8cad97becda06e3d872578
konsmosc <kostasmoschou@gmail.com>
2022-04-13 10:58:25 +0300

    update voting.sol

--- voting.sol
+++ voting.sol
@@ -7,13 +7,6 @@ contract Voting {
     address private owner;
     mapping (bytes32 => uint) private votesReceived;
     bytes32[] public candidateList;
-    mapping (address => bool) private voters;
-    string public votetitle;
-
-    modifier onlyOwner(){
-        require(msg.sender == owner,"Only owner can call this function.");
-        _;
-    }
 
     constructor(bytes32[] memory candidateNames, string memory title) {
         candidateList = candidateNames;
@@ -23,14 +16,9 @@ contract Voting {
 
     function voteForCandidate(bytes32 candidate) public {
         require(validCandidate(candidate) == true,"Candidate doesn't exists");
-        require(!voters[msg.sender], "You have already submit your vote");
         votesReceived[candidate] += 1;
-        voters[msg.sender] = true;
     }
 
-    function changeTitle(string memory title) public onlyOwner {
-        votetitle = title;
-    }
 
     function totalVotesFor(bytes32 candidate) public view returns (uint) {
         require(validCandidate(candidate) == true, "Candidate doesn't exists");
